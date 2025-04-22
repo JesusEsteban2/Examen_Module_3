98cbe6844fbb0dba20978ada2943bdf93fe6d707
Mananas <Mananas>
2025-04-22 13:38:34 +0200

    Update Test

--- src/front/components/form.add.test.ts
+++ src/front/components/form.add.test.ts
@@ -1,14 +1,15 @@
 import { createFormAdd } from './form.add';
 import { screen } from '@testing-library/dom';
-import '@testing-library/jest-dom'; //nameSpace for jest-dom matchers
+import '@testing-library/jest-dom';
 
 describe('Given createFormAdd', () => {
     describe('When is instantiated  ', () => {
-        test('Them a instance be create', async () => {
-            const form = createFormAdd([], 'body', 'afterbegin');
+        const form = createFormAdd([], 'body', 'afterbegin');
 
+        test('Them a instance be create', async () => {
             expect(form).toBeInstanceOf(HTMLFormElement);
         });
+
         test('Then a Form be in the document', () => {
             const formElement = screen.getByRole('form', {
                 name: 'add_form',
@@ -19,7 +20,6 @@ describe('Given createFormAdd', () => {
 
         test('Then have a button in document', () => {
             const button = screen.getByRole('button') as HTMLElement;
-
             expect(button).toBeInTheDocument();
             expect(button).toHaveAttribute('type', 'submit');
         });
