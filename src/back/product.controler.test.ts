05c4576072cb54ebc34d4ef784bbc11bf6aa313e
Mananas <Mananas>
2025-04-22 12:04:14 +0200

    Create test

--- src/back/product.controler.test.ts
+++ src/back/product.controler.test.ts
@@ -1,44 +1,85 @@
 import { NextFunction, Request, Response } from 'express';
 import { ProductsController } from './products.controller';
-import createDebug from 'debug';
-import type { AppResponse } from '../types/app-response';
-import type { ProductCreateDTO } from './dto/products.dto';
-import { Product } from '@prisma/client';
-import { ProductRepo } from '../repo/products.repository.js';
 import { vi } from 'vitest';
+import { error } from 'console';
 
-
-const mockRepo={
-    getAll: vi.fn(),
-
-} as ProductRepo;
+const mockRepo = {
+    read: vi.fn().mockResolvedValueOnce({}).mockRejectedValueOnce(error),
+    readById: vi.fn().mockResolvedValueOnce({}).mockRejectedValueOnce(error),
+    create: vi.fn().mockResolvedValueOnce({}).mockRejectedValueOnce(error),
+    update: vi.fn().mockResolvedValueOnce({}).mockRejectedValueOnce(error),
+    delete: vi.fn().mockResolvedValueOnce({}).mockRejectedValueOnce(error),
+};
 
 const next = vi.fn() as NextFunction;
-const req = {} as Request;
-const res = {} as Response;
+const req = {
+    params: { id: '1' },
+} as unknown as Request;
 
-
+const res = {
+    json: vi.fn(),
+} as unknown as Response;
 
 describe('Given productControler', () => {
-
-    const producControler=new ProductsController(mocRepo);
+    const producControler = new ProductsController(mockRepo);
 
     describe('Wen instance', () => {
         test('Then have a instance of ProductsController', () => {
-            expect (producControler).toBeInstanceOf(ProductsController);
+            expect(producControler).toBeInstanceOf(ProductsController);
         });
     });
 
-    describe('Wen getAll is called',()=>{
-        test('',()=>{
-            producControler.getAll(req,res,next)
-            expect
-        })
+    describe('Wen getAll is called', () => {
+        test('Then json has been called', async () => {
+            await producControler.getAll(req, res, next);
+            expect(res.json).toHaveBeenCalledWith({
+                error: '',
+                results: {},
+            });
+            await producControler.getAll(req, res, next);
+            expect(next).toHaveBeenCalledWith(error);
+        });
     });
 
+    describe('Wen getById is called', () => {
+        test('Then json has been called', async () => {
+            await producControler.getById(req, res, next);
+            expect(res.json).toHaveBeenCalledWith({
+                error: '',
+                results: {},
+            });
+            await producControler.getById(req, res, next);
+            expect(next).toHaveBeenCalledWith(error);
+        });
+    });
 
-    describe('',()=>{,()=>
-    describe('',()=>{,()=>
+    describe('Wen create is called', () => {
+        test('Then json has been called', async () => {
+            await producControler.create(req, res, next);
+            expect(res.json).toHaveBeenCalledWith({
+                error: '',
+                results: {},
+            });
+            await producControler.create(req, res, next);
+            expect(next).toHaveBeenCalledWith(error);
+        });
+    });
 
-    })
+    describe('Wen update is called', () => {
+        test('Then json has been called', async () => {
+            await producControler.update(req, res, next);
+            expect(res.json).toHaveBeenCalled();
+            await producControler.update(req, res, next);
+            expect(next).toHaveBeenCalledWith(error);
+        });
+    });
+
+    describe('Wen delete is called', () => {
+        test('Then json has been called', async () => {
+            await producControler.delete(req, res, next);
+            expect(res.json).toHaveBeenCalled();
+            await producControler.delete(req, res, next);
+            expect(next).toHaveBeenCalledWith(error);
+        });
+    });
 });
